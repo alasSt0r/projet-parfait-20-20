248aca6c6a1b93cf2750ed686d91c12148ece963
alasSt0r <root@fedora.marius>
2025-03-04 14:15:30 +0100

    init

---
+++ vue/vueAccueil.php
@@ -0,0 +1,10 @@
+
+        <div class="menu">
+            <div class="roman clickable"><a href="./livre/roman/roman.html">Roman</a></div>
+            <div class="sf clickable"><a href="./livre/sci-fi/sci-fi.html">Science-Fiction</a></div>
+            <div class="bd clickable"><a href="./livre/bd/bd.html">Bandes-Déssinnée</a></div>
+            <div class="manga clickable"><a href="./livre/manga/manga.html">Manga</a></div>
+            <div class="docu clickable"><a href="./livre/docu/docu.html">Documentaires</a></div>
+        </div>
+    </body>
+</html>
