db8d7f2f16c10b6d2325308ceefbcde8b07664e2
alasSt0r <94861150+alasSt0r@users.noreply.github.com>
2025-03-07 08:48:26 +0100

    add contact vue

--- vue/vueContact.php
+++ vue/vueContact.php
@@ -1,2 +1,37 @@
-<h1>Contact</h1>
-<p> Ceci est la page de contact</p>
+<div class="menucontact">
+        <h1>Contact</h1>
+        <div class="contenu">
+            <span class="co1">
+                <div>
+                    Nom : <input type="text">
+                </div>
+                <div>
+                    @ Mail : <input type="text">
+                </div>
+                <div>
+                    N° Tel : <input type="text">
+                </div>
+                <div>
+                    Message : <input type="text">
+                </div>
+                <div>
+                    <button class="button">Envoyer</button>
+                </div>
+            </span>
+            <span class="co2">
+                <div>
+                    Nos Coordonées
+                </div>
+                <div>
+                    N° Telephone : 03 20 44 45 56
+                </div>
+                <div>
+                    Adresse Mail : LibraryWebSite@gmail.com
+                </div>
+                <div>
+                    Emplacement : 3 Rue de la Victoire 59000 (Lille)
+                </div>
+            </span>
+        </div>
+    </div>
+</body>
