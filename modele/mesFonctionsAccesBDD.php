db84a9eb316b18f87491c545c6101ea269b87694
alasSt0r <94861150+alasSt0r@users.noreply.github.com>
2025-03-11 14:42:08 +0100

    deleted unsafe pdo access

--- modele/mesFonctionsAccesBDD.php
+++ modele/mesFonctionsAccesBDD.php
@@ -10,14 +10,6 @@
         return $unObjPDO;
     }
 
-    // Fonction qui execute la requete $requete passée en paramètre et renvoie le tableau de résultat
-    function dbRequest($unobjPDO, $requete){
-        $unObjPDOStatement = $unobjPDO->prepare($requete);
-        $unObjPDOStatement->execute();
-        $unObjPDOQuery = $unObjPDOStatement->fetchAll;
-        return $unObjPDOQuery;
-    }
-
     // Fonction qui ferme la connexion à la base de données
     function dbDisconnect($unObjPDO){
         $unObjPDO = null;
