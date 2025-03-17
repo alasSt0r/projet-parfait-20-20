ac1e08a984fbc3a06decdeb2fc86c8a81aba56aa
alasSt0r <94861150+alasSt0r@users.noreply.github.com>
2025-03-17 13:10:23 +0100

    add search using $_get method

--- controleur/chercher.php
+++ controleur/chercher.php
@@ -5,8 +5,15 @@
 
 // Partie de traitement des données récupérées si besoin pour mise à disposition de la vue
 
+if (isset($_GET['titre'])) {
+    $recherche = $_GET['titre'];
 
+    //Instanciation de la connexion 
+    $unObjPDO = dbConnection();
 
+    //Appel de la fonction getLivresByTitre
+    $lesLivres = getLivresByTitre($unObjPDO, $recherche);
+}
 
 // appel du script de vue qui permet de gerer l'affichage des donnees
 include "vue/vueChercher.php";
--- vue/vueChercher.php
+++ vue/vueChercher.php
@@ -1,6 +1,19 @@
 <div class="menuchercher">
-    <form action="./autre/fonctionRechercheForm" method="POST">
-        <input class ="barrerecherche" type="text" name="recherche" placeholder="Rechercher" required>
-        <button class="boutonrecherche"type="submit" name="submit"><img class="loupe" src="./img/loupe.png"/></button>
+    <form method="GET">
+        <input class ="invisible" type="text" name="action" value="chercher">
+        <input class="barrerecherche" type="text" name="titre" placeholder="Rechercher" required>
+        <button class="boutonrecherche" type="submit" name="submit"><img class="loupe" src="./img/loupe.png" /></button>
     </form>
+
+    <div class="resultatrecherche">
+        <ul>
+            <?php
+            if (isset($lesLivres)) {
+            foreach ($lesLivres as $unLivre) {
+                echo "<li><a href='./index.php?page=detail&isbn=" . $unLivre['isbn'] . "'>" . $unLivre['titre'] . "</a></li>";
+            }
+        }
+            ?>
+        </ul>
+    </div>
 </div>
