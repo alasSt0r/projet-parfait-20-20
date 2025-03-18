8cbe54fec00be635e1e5d751724d484814e72486
alasSt0r <94861150+alasSt0r@users.noreply.github.com>
2025-03-18 11:04:27 +0100

    Fix query in getGenres function to select from Genres table

--- modele/mesFonctionsAccesBDD.php
+++ modele/mesFonctionsAccesBDD.php
@@ -40,7 +40,7 @@
 
     // Fonction qui retourne la liste des genres
     function getGenres($unObjPDO){
-        $requete = "SELECT DISTINCT genre FROM Livres";
+        $requete = "SELECT DISTINCT genre FROM Genres";
         $stmt = $unObjPDO->prepare($requete);
         $stmt->execute();
         $lesGenres = $stmt->fetchAll();
