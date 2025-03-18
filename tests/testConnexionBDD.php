14313f86edbac89f0d998d3e13cb5f1e0c7d4ec9
alasSt0r <94861150+alasSt0r@users.noreply.github.com>
2025-03-18 10:44:07 +0100

    ajout d'un script de test a la connexion bdd

---
+++ tests/testConnexionBDD.php
@@ -0,0 +1,8 @@
+<?php
+    // Test de connexion à la base de données
+    include "../modele/mesFonctionsAccesBDD.php";
+    $unObjPDO = dbConnection();
+    var_dump($unObjPDO);
+    dbDisconnect($unObjPDO);
+    var_dump($unObjPDO);
+?>
